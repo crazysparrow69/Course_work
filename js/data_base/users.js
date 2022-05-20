2a542e27577c17c2b99dfe50c28946b575f007b8
Denys <denissytnikmacho@gmail.com>
2022-05-20 16:25:12 +0300

    added auto-searching in local storage

--- js/data_base/users.js
+++ js/data_base/users.js
@@ -1,3 +1,14 @@
 "use strict"
 
 export const users = [];
+
+const addUser = () => {
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    const obj = JSON.parse(localStorage.getItem(key));
+    if (obj.type === "user") users.push(obj);
+  }
+};
+
+addUser();
+console.log(users);
