55e9e1d5675eed479bd600bab984f9749db7145a
Denys <denissytnikmacho@gmail.com>
2022-06-02 15:07:11 +0300

    finished script for loading content

--- js/film_page.js
+++ js/film_page.js
@@ -1,16 +1,70 @@
 "use strict"
 
 import { page } from "./data_base/page.js";
+import { addFilmToUserInLS } from "./films_actions.js";
+import { popup } from "./modal_window.js";
 
-function deleteFilmFromLS() {
+let film = null;
+
+function findFilmInLS() {
   for (let i = 0; i < localStorage.length; i++) {
     const key = localStorage.key(i);
     const obj = JSON.parse(localStorage.getItem(key));
     if (obj.type !== "user") {
-      localStorage.removeItem(key);
+      film = obj;
     }
   }
 }
 
-deleteFilmFromLS();
-console.log(page.film);
+function loadAvatar() {
+  const avatar = document.getElementById("avatar");
+  avatar.src = film.img;
+}
+
+function loadTitle() {
+  const title = document.getElementById("title");
+  title.innerHTML = film.name;
+}
+
+function loadYear() {
+  const year = document.getElementById("year");
+  year.innerHTML = film.year;
+}
+
+function loadGenre() {
+  const genre = document.getElementById("genre");
+  genre.innerHTML = film.genre;
+}
+
+function removeFilmFromLS() {
+  const key = film.name;
+  localStorage.removeItem(key);
+}
+
+function func() {
+  findFilmInLS();
+  loadAvatar();
+  loadTitle();
+  loadYear();
+  loadGenre();
+  removeFilmFromLS();
+}
+
+func();
+
+// Open profile ///////////////////////////////////////////////////////////////////
+const buttonProfile = document.getElementById("button-profile");
+
+const openProfile = () => {
+  if (page.status === "logged") return window.location.href = "/html/profile.html";
+  popup("Вы не вошли в аккаунт");
+};
+
+buttonProfile.addEventListener("click", openProfile);
+
+// Add film to chosens /////////////////////////////////////////////////////
+document.getElementById("chosens-button").addEventListener("click", () => { 
+  addFilmToUserInLS(page.user.name, "chosens", film.name);
+  addFilmToUserInLS(page.user.name, "films", film.name); 
+  popup("Фильм добавлен в избранное");
+})
