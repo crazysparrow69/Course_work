e1b555fe3f750cad433720a6b75307caa064c2a2
Denys <denissytnikmacho@gmail.com>
2022-06-26 14:06:21 +0300

    deleted eslint due to some problems

--- js/fill_content.js
+++ js/fill_content.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-undef */
+/* eslint-disable import/prefer-default-export */
 // Fill "divClass" container with "array" elements
 export const createFilmDiv = (divClass, array) => {
   const div = document.getElementById(divClass);
